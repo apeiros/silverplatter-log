ed0c4e2882866eb0f9b4a45de05aee2fc346d927
Stefan Rusterholz <stefan@Schrulle.local>
2008-07-05 21:25:15 +0200

    added log/version

---
+++ trunk/lib/silverplatter/log/version.rb
@@ -0,0 +1,14 @@
+#--
+# Copyright 2007 by Stefan Rusterholz.
+# All rights reserved.
+# See LICENSE.txt for permissions.
+#++
+
+
+
+module SilverPlatter
+	module Log
+		# The version of silverplatter-log
+		VERSION = "0.1.0"
+	end # Log
+end # SilverPlatter
